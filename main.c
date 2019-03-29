dbacd4b6a62daa421bc939243fa44c3231bbf161
cqkeenan <48570329+cqkeenan@users.noreply.github.com>
2019-03-28 23:42:04 -0400

    Update 3/28 10:40pm
    adding movies, displaying movies functional in new and existing log files. still need to complete delete and update records.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "scanner.h"
 #include "movie.h"
 
@@ -37,7 +38,7 @@ interface:
 	printf("2. Display existing catalog\n");
 	printf("3. Update existing catalog\n");
 	printf("4. Exit program\n\n");
-	scanf("%c", &choice);
+	choice = readChar(stdin);
 	switch(choice) {
 		case '1' :
 			goto create;
@@ -64,12 +65,12 @@ create: ;
 	char *filename = strcat(name, extension);
 	FILE *userFile;
 	userFile = fopen(filename, "w");
-addMovie:
+addMovie: ;
 	char *search;
 	int matches = 0;
 	int matchFound = 0;
 	printf("Search for a movie: ");
-	while((getchar() != '\n'));
+	while ((getchar() != '\n'));
 	search = readLine(stdin);
 	for(int val = 0; val < i; val++) {
 		matchFound = contains(a.array[val].primaryTitle, search);
@@ -78,24 +79,125 @@ addMovie:
 			++matches;
 		}
 		if(matches == 25) {
-			printf("First %d matches displayed. If you do not see your movie, please enter a more specific search.\n", matches);
+			printf("\nFirst %d matches displayed. If you do not see your movie, please enter a more specific search.\n\n", matches);
 			break;
 		}
 	}
+	if(matches == 0) {
+		printf("No matches found. please try another search.\n\n");
+		goto addMovie;
+	}
+enterNum: ;
+	int movieNum = 0;
+	printf("To add a movie to your log file, type the number associated with the movie you wish to add. If you do not wish to add any of the movies listed, type \"0\" to see more options.\n\n");
+	printf("WARNING: Selecting a number not listed above could either add a random movie to your log.\n\n");
+	movieNum = readInt(stdin);
+	if(movieNum == 0) goto interface;
+	else if(movieNum >= i) {
+		printf("The number you entered is outside the bounds of the database. please enter a valid number.\n\n");
+		goto enterNum;
+	}
+	else {
+		printf("Adding the following movie:\n\n");
+		printMovie(&a, movieNum-1);
+		char *mediaType;
+media:
+		printf("\nWhat is the media type for your movie? (Type the number of your choice.)\n");
+		printf("1. DVD\n2. Bluray\n3. Digital\n");
+		choice = readChar(stdin);
+		if(choice < '1' || choice > '3') {
+			printf("Invalid choice, please enter a valid choice.\n\n");
+			goto media;
+		}
+		if(choice == '1') mediaType = "DVD";
+		else if(choice == '2') mediaType = "Bluray";
+		else mediaType = "Digital";
+		int month, day, year;
+dateEntry:
+		printf("Did you acquire this movie today? (Type \"Y\" for yes, \"N\" for no.)\n");
+		choice = readChar(stdin);
+		if(choice == 'Y') {
+			time_t now;
+			time(&now);
+			struct tm *local = localtime(&now);
+			month = local->tm_mon + 1;
+			day = local->tm_mday;
+			year = local->tm_year + 1900;
+		}
+		else if(choice == 'N') {
+			printf("Enter the number representing the month you acquired the movie.\n");
+			month = readInt(stdin);
+			printf("Enter the day you acquired the movie.\n");
+			day = readInt(stdin);
+			printf("Enter the year you acquired the movie.\n");
+			year = readInt(stdin);
+		}
+		else {
+			printf("Invalid choice, please either enter \"Y\" or \"N\".\n\n");
+			goto dateEntry;
+		}
+		fprintf(userFile, "%s\t%s\t%s\t%s\t%s\t%02d/%02d/%d\n", a.array[movieNum-1].primaryTitle, a.array[movieNum-1].genres, a.array[movieNum-1].runtimeMinutes, a.array[movieNum-1].startYear, mediaType, month, day, year);
+	}
+	printf("Movie added to log file successfully.\n\n");
 	fclose(userFile);
-	goto interface;
+	goto updateMenu;
 display:
-	printf("FIXME D\n\n");
-	goto interface;
+	printf("Enter the name of your log file (format is {yourfilenamehere}.log): ");
+        name = readToken(stdin);
+        printf("Opening %s.\n", name);
+        userFile = fopen(name, "r");
+        if(userFile == 0) {
+                printf("Error: File \"%s\" could not be found. Returning to main menu.\n\n", name);
+                goto interface;
+        }
+displayMovies:
+	char c = fgetc(userFile);
+	while (c != EOF) {
+		printf("%c", c);
+		c = fgetc(userFile);
+	}
+	fclose(userFile);
+	goto updateMenu;
 update:
 	printf("Enter the name of your log file (format is {yourfilenamehere}.log): ");	
 	name = readToken(stdin);
 	printf("Opening %s.\n", name);
-	userFile = fopen(name, "rw");
-	if(userFile == NULL) {
+	userFile = fopen(name, "r");
+	if(userFile == 0) {
 		printf("Error: File \"%s\" could not be found. Returning to main menu.\n\n", name);
 		goto interface;
 	}
+	fclose(userFile);
+updateMenu:
+	printf("What would you like to do?\n\n");
+	printf("1. Add a movie\n2. Delete a movie\n3. Update existing movie\n4. Display current catalog\n5. Return to main menu\n\n");
+	choice = readChar(stdin);
+	switch(choice) {
+		case '1' :
+			userFile = fopen(name, "a");
+			goto addMovie;
+		case '2' :
+			userFile = fopen(name, "rw");
+			goto deleteMovie;
+		case '3' :
+			userFile = fopen(name, "rw");
+			goto updateMovie;
+		case '4' :
+			userFile = fopen(name, "r");
+			goto displayMovies;
+		case '5' :
+			fclose(userFile);
+			goto interface;
+		default :
+			printf("Invalid choice, please try again.\n\n");
+			goto updateMenu;
+	}
+deleteMovie:
+	printf("FIX DELETE\n");
+	goto updateMenu;
+updateMovie:
+	printf("FIX DELETE\n");
+	goto updateMenu;
 end:
 	freeArray(&a);
 	return 0;
